1736155baa3a4e368398eec6e5821d6c6a542398
romnick <romnicksanjuan9@gmail.com>
2024-02-01 17:59:41 +0800

    a

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin:['https://student-crud-eta.vercel.app/'],
+        origin:['https://student-crud-eta.vercel.app'],
         methods:['POST', 'GET'],
         credentials:true
     }

ff82f9036822d543638caaf8e33a14fe7d13d590
romnick <romnicksanjuan9@gmail.com>
2024-02-01 18:31:11 +0800

    aaa

--- server/server.js
+++ server/server.js
@@ -3,15 +3,15 @@ const connectDB = require('./config/db.js')
 const dotenv = require('dotenv');
 const cors = require('cors')
 const studentRegister = require('./routes/student_route.js')
-// const studentLogin = require('./routes/login.js')
+const studentLogin = require('./routes/login.js')
 const Student = require('./model/student.js')
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 dotenv.config();
 
 const app = express();
 
-mongoose.connect('mongodb+srv://romnick:1234@romnickdb.e14diyv.mongodb.net/student_crud')
-.then(res => console.log("connected to db"))
+// mongoose.connect('mongodb+srv://romnick:1234@romnickdb.e14diyv.mongodb.net/student_crud')
+// .then(res => console.log("connected to db"))
 
 app.use(cors(
     {
@@ -22,20 +22,11 @@ app.use(cors(
 ))
 app.use(express.json());
 app.use('/student_crud', studentRegister)
-// app.use('/', studentLogin);
-
-app.use('/student_crud/read', async (req,res) =>{
-    try {
-        const data = await Student.find({});
-        res.json(data)
-    } catch (error) {
-        console.log(error)
-    }
-})
+app.use('/', studentLogin);
 
 
 const port = process.env.PORT
 app.listen((port), () =>{
-    
+    connectDB();
     console.log(`server is running on port ${port}`)
 })

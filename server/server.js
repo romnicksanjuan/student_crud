909edc6e1314cfc9ca38cf59d199993ffd5f71d8
romnick <romnicksanjuan9@gmail.com>
2024-02-01 18:38:54 +0800

    aasdsdsdsd

--- server/server.js
+++ server/server.js
@@ -5,13 +5,13 @@ const cors = require('cors')
 const studentRegister = require('./routes/student_route.js')
 const studentLogin = require('./routes/login.js')
 const Student = require('./model/student.js')
-// const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 dotenv.config();
 
 const app = express();
 
-// mongoose.connect('mongodb+srv://romnick:1234@romnickdb.e14diyv.mongodb.net/student_crud')
-// .then(res => console.log("connected to db"))
+mongoose.connect('mongodb+srv://romnick:1234@romnickdb.e14diyv.mongodb.net/student_crud')
+.then(res => console.log("connected to db"))
 
 app.use(cors(
     {
@@ -25,8 +25,7 @@ app.use('/student_crud', studentRegister)
 app.use('/', studentLogin);
 
 
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 app.listen((port), () =>{
-    connectDB();
     console.log(`server is running on port ${port}`)
 })
